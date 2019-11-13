2baee0979457c7bb8b2d4c4d009b72c9571e8758
Joachim Metz <joachim.metz@gmail.com>
2019-11-13 08:07:50 +0100

    Applied updates

--- .travis/script.sh
+++ .travis/script.sh
@@ -25,7 +25,7 @@ then
 
 		make install DESTDIR=${PWD}/osx-pkg;
 		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libcdirectory;
-		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libcdirectory;
+		cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libcdirectory;
 
 		pkgbuild --root osx-pkg --identifier com.github.libyal.libcdirectory --version ${VERSION} --ownership recommended ../libcdirectory-${VERSION}.pkg;
 	fi
